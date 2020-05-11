eeb9a1772862b52bf3093370ee8ccd3dad973511
Ashank <yohaw1@gmail.com>
2020-05-11 15:03:47 -0400

    removed uneeded comments

--- corona2.py
+++ corona2.py
@@ -1,19 +1,6 @@
 import plotly.graph_objects as go
 import pandas as pd
 
-# def sort_geocord(data):
-#     #Create list of pair of rows to swap
-#     #then swaps them
-#     duplicate_cities = data.duplicated(['Cities'], keep=False)
-
-
-
-# def swap_pd_rows(rows_to_swap):
-#     #takes a list of pairs of rows to swap and 
-#     #swaps them
-
-
-
 state_data = pd.read_csv('StateData.csv')
 prison_data = pd.read_csv('test3.csv') #too many overlapping points
 prison_data = prison_data.sort_values(by = 'Inmates Positive', ascending=True)
@@ -28,22 +15,26 @@ choro = go.Choropleth(
         # [0, "rgb(0, 0, 0)"],
         # [0.9, "rgb(200, 100, 100)"],
 
-        [0, 'red'],
-        [0.05, 'red'],
-        [0.05, 'blue'],
-        [0.1, 'blue'],
-        [0.1, 'green'],
-        [0.2, 'green'],
-        [0.2, 'orange'],
-        [1, 'orange']
+        # [0, 'red'],
+        # [0.05, 'red'],
+        # [0.05, 'blue'],
+        # [0.1, 'blue'],
+        # [0.1, 'green'],
+        # [0.2, 'green'],
+        # [0.2, 'orange'],
+        # [1, 'orange']
+
+        [0, '#BF5900'],
+        [0.05, '#AB5C16'],
+        [0.05, '#72665B'],
+        [0.1, '#4C6C89'],
+        [0.1, '#3970A0'],
+        [0.2, '#2673B7'],
+        [0.2, '#1376CE'],
+        [1, '#007AE5']
     ],
     colorbar_title = 'Thousands People',
     zauto=False,
-    # zmax=int(state_data['Cases'].max()),
-    # zmin=int(state_data['Cases'].median())
-    # zmax=int(state_data['Cases'].max()),
-    # zmin=int(state_data['Cases'].min()),
-    # zmid=int(state_data['Cases'].median())
 )
 
 scatter = go.Scattergeo(
@@ -54,6 +45,6 @@ scatter = go.Scattergeo(
     marker=dict(size=12, color=prison_data['Inmates Positive'], colorscale='Reds') #Need to add colorscale trace for scatters
 )
 
-fig = go.Figure(data=[choro, ],) #scatter
+fig = go.Figure(data=[choro, scatter],) #scatter
 fig.update_layout(geo_scope='usa') 
 fig.show()
